f869a5cd513c412502ab6e522febbd32831c26bb
sidgharge <ghargesiddharth@gmail.com>
2018-12-24 22:21:24 +0530

    Added feign method

--- gateway/src/main/java/com/microservices/gateway/LoginClient.java
+++ gateway/src/main/java/com/microservices/gateway/LoginClient.java
@@ -3,7 +3,10 @@ package com.microservices.gateway;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @FeignClient(name="security-service")
 @RibbonClient(name="security-service")
@@ -11,4 +14,8 @@ public interface LoginClient {
 
 	@GetMapping("/login")
 	String login(@RequestParam("user") String user, @RequestParam("password") String password);
+	
+	// TODO
+	@GetMapping()
+	ObjectNode getDocs();
 }
